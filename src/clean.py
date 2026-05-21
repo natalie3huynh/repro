2de622b5b3a4e991f1765ca0218b603633a4d4a3
Natalie Huynh <n3huynh@ucsd.edu>
2026-05-21 15:58:50 -0700

    complete dvc pipeline

--- src/clean.py
+++ src/clean.py
@@ -5,6 +5,9 @@ os.makedirs("data/clean", exist_ok=True)
 
 df = pd.read_csv("data/raw/events.csv")
 
+df = df.dropna()
+df = df[~df.eq("").any(axis=1)]
+
 df["event_type"] = df["event_type"].astype(str).str.lower().str.strip()
 
 valid_event_types = {"click", "login", "purchase", "scroll", "view"}
